1ce72e04b8cf55f17859b94865a99e250878500b
Neurs <31346263+neursh@users.noreply.github.com>
2024-08-13 20:47:51 +0700

    Make code efficient

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-interface CompleteJsonRequest {
-	name: string;
-	token: string;
-	trackId: string;
-	partsCount: number;
-}
-
-interface MultipartJsonRequest {
-	key: string;
-}
-
 export interface Env {
 	KV: KVNamespace;
 	BUCKET: R2Bucket;
@@ -18,48 +7,82 @@ interface RouteOptions {
 	startswithCheck: boolean;
 }
 
-class RouteBuilder {
+class Route {
 	pathname: string;
-	private model: () => Promise<Response>;
+	private model: (request: Request, env: Env, ctx: ExecutionContext, state: any) => Promise<Response>;
 	options: RouteOptions;
 
-	constructor(pathname: string, model: () => Promise<Response>, options: RouteOptions) {
+	constructor(pathname: string, model: (request: Request, env: Env, ctx: ExecutionContext, state: any) => Promise<Response>, options: RouteOptions) {
 		this.pathname = pathname;
 		this.model = model;
 		this.options = options;
 	}
 
-	async response(): Promise<Response> {
-		return this.model();
+	async response(request: Request, env: Env, ctx: ExecutionContext, state: any): Promise<Response> {
+		return this.model(request, env, ctx, state);
 	}
 }
 
-class WorkersApp {
-	private getRoutes: RouteBuilder[] = [];
-	private postRoutes: RouteBuilder[] = [];
+class WorkersRouter {
+	private routes = {
+		GET: <Route[]>[],
+		HEAD: <Route[]>[],
+		POST: <Route[]>[],
+		PUT: <Route[]>[],
+		DELETE: <Route[]>[],
+		CONNECT: <Route[]>[],
+		OPTIONS: <Route[]>[],
+		TRACE: <Route[]>[],
+		PATCH: <Route[]>[],
+	};
 
-	get(pathname: string, model: () => Promise<Response>, options: RouteOptions = { startswithCheck: false }) {
-		this.getRoutes.push(new RouteBuilder(pathname, model, options));
+	get(pathname: string, model: (request: Request, env: Env, ctx: ExecutionContext, state: any) => Promise<Response>, options: RouteOptions = { startswithCheck: false }) {
+		this.routes.GET.push(new Route(pathname, model, options));
 	}
 
-	post(pathname: string, model: () => Promise<Response>, options: RouteOptions = { startswithCheck: false }) {
-		this.postRoutes.push(new RouteBuilder(pathname, model, options));
+	head(pathname: string, model: (request: Request, env: Env, ctx: ExecutionContext, state: any) => Promise<Response>, options: RouteOptions = { startswithCheck: false }) {
+		this.routes.HEAD.push(new Route(pathname, model, options));
 	}
 
-	async handle(pathname: string, method: string): Promise<Response> {
-		let targetRoutes: RouteBuilder[] | null = null;
+	post(pathname: string, model: (request: Request, env: Env, ctx: ExecutionContext, state: any) => Promise<Response>, options: RouteOptions = { startswithCheck: false }) {
+		this.routes.POST.push(new Route(pathname, model, options));
+	}
 
-		if (method === "GET") {
-			targetRoutes = this.getRoutes;
-		} else if (method === "POST") {
-			targetRoutes = this.postRoutes;
-		}
+	put(pathname: string, model: (request: Request, env: Env, ctx: ExecutionContext, state: any) => Promise<Response>, options: RouteOptions = { startswithCheck: false }) {
+		this.routes.PUT.push(new Route(pathname, model, options));
+	}
 
-		if (targetRoutes !== null) {
+	delete(pathname: string, model: (request: Request, env: Env, ctx: ExecutionContext, state: any) => Promise<Response>, options: RouteOptions = { startswithCheck: false }) {
+		this.routes.DELETE.push(new Route(pathname, model, options));
+	}
+
+	connect(pathname: string, model: (request: Request, env: Env, ctx: ExecutionContext, state: any) => Promise<Response>, options: RouteOptions = { startswithCheck: false }) {
+		this.routes.CONNECT.push(new Route(pathname, model, options));
+	}
+
+	options(pathname: string, model: (request: Request, env: Env, ctx: ExecutionContext, state: any) => Promise<Response>, options: RouteOptions = { startswithCheck: false }) {
+		this.routes.OPTIONS.push(new Route(pathname, model, options));
+	}
+
+	trace(pathname: string, model: (request: Request, env: Env, ctx: ExecutionContext, state: any) => Promise<Response>, options: RouteOptions = { startswithCheck: false }) {
+		this.routes.TRACE.push(new Route(pathname, model, options));
+	}
+
+	patch(pathname: string, model: (request: Request, env: Env, ctx: ExecutionContext, state: any) => Promise<Response>, options: RouteOptions = { startswithCheck: false }) {
+		this.routes.PATCH.push(new Route(pathname, model, options));
+	}
+
+	async handle(request: Request, env: Env, ctx: ExecutionContext, state: any): Promise<Response> {
+		const method = request.method;
+		const { pathname } = new URL(request.url);
+
+		let targetRoutes: Route[] | undefined = this.routes[method as keyof typeof this.routes];
+
+		if (targetRoutes && targetRoutes.length > 0) {
 			for (let routeIter = 0; routeIter < targetRoutes.length; routeIter++) {
 				const route = targetRoutes[routeIter];
 				if (route.options.startswithCheck ? pathname.startsWith(route.pathname) : pathname === route.pathname) {
-					return route.response();
+					return route.response(request, env, ctx, state);
 				}
 			}
 		}
@@ -68,7 +91,10 @@ class WorkersApp {
 	}
 }
 
-async function checkHosting(auth: any, env: Env): Promise<boolean> {
+async function checkAuth(auth: string[] | undefined, env: Env): Promise<boolean> {
+	if (!auth) {
+		return false;
+	}
 	if (auth[0] === "lastRun" || auth[0] === "sessionHostOwner") {
 		return false;
 	}
@@ -78,140 +104,162 @@ async function checkHosting(auth: any, env: Env): Promise<boolean> {
 	return false;
 }
 
+const app = new WorkersRouter();
+
+app.get("/session/createMultipartUpload", async (_, env, __, state) => {
+	const user = state.auth[0];
+	const sessionHostOwner = await env.KV.get("sessionHostOwner");
+
+	if (sessionHostOwner === user) {
+		const multipartUpload = await env.BUCKET.createMultipartUpload(state.multipartGlobalKey);
+		return Response.json({
+			key: multipartUpload.key,
+			uploadId: multipartUpload.uploadId,
+		});
+	}
+
+	return new Response(null, { status: 404 });
+});
+
+app.get("/session/createMultipartUpload", async (_, env, __, state) => {
+	const user = state.auth[0];
+	const sessionHostOwner = await env.KV.get("sessionHostOwner");
+
+	if (sessionHostOwner === user) {
+		const multipartUpload = await env.BUCKET.createMultipartUpload(state.multipartGlobalKey);
+		return Response.json({
+			key: multipartUpload.key,
+			uploadId: multipartUpload.uploadId,
+		});
+	}
+
+	return new Response(null, { status: 404 });
+});
+
+app.post("/session/uploadPart", async (request, env, __, state) => {
+	const user = state.auth[0];
+	const sessionHostOwner = await env.KV.get("sessionHostOwner");
+	const uploadId = state.searchParams.get("uploadId"),
+	part = parseInt(state.searchParams.get("part") ?? "");
+
+	if (sessionHostOwner === user && uploadId && !Number.isNaN(part) && request.body) {
+		const multipartUpload = env.BUCKET.resumeMultipartUpload(state.multipartGlobalKey, uploadId);
+		const uploadedPart: R2UploadedPart = await multipartUpload.uploadPart(part, request.body);
+
+		return Response.json(uploadedPart);
+	}
+
+	return new Response(null, { status: 404 });
+}, { startswithCheck: true });
+
+app.post("/session/uploadComplete", async (request, env, _, state) => {
+	const user = state.auth[0];
+	const sessionHostOwner = await env.KV.get("sessionHostOwner");
+	const uploadId = state.searchParams.get("uploadId");
+
+	if (sessionHostOwner === user && uploadId) {
+		const multipartUpload = env.BUCKET.resumeMultipartUpload(state.multipartGlobalKey, uploadId);
+
+		await multipartUpload.complete(await request.json());
+
+		return new Response();
+	}
+
+	return new Response(null, { status: 404 });
+}, { startswithCheck: true });
+
+app.get("/session/uploadAbort", async (_, env, __, state) => {
+	const user = state.auth[0];
+	const sessionHostOwner = await env.KV.get("sessionHostOwner");
+	const uploadId = state.searchParams.get("uploadId");
+
+	if (sessionHostOwner === user && uploadId) {
+		const multipartUpload = env.BUCKET.resumeMultipartUpload(state.multipartGlobalKey, uploadId);
+
+		await multipartUpload.abort();
+
+		return new Response();
+	}
+
+	return new Response(null, { status: 404 });
+}, { startswithCheck: true });
+
+app.get("/session/stop", async (_, env, __, state) => {
+	const user = state.auth[0];
+	const sessionHostOwner = await env.KV.get("sessionHostOwner");
+
+	if (sessionHostOwner === user) {
+		await env.KV.delete("sessionHostOwner");
+
+		const time = Math.floor(Date.now() / 1000).toString();
+		await env.KV.put("lastRun", time);
+
+		return Response.json({ time: time });
+	}
+
+	return new Response(null, { status: 404 });
+});
+
+app.get("/session/check", async (_, env, __, ___) => {
+	const sessionHostOwner = await env.KV.get("sessionHostOwner");
+	if (sessionHostOwner === null) {
+		const lastRun = await env.KV.get("lastRun") ?? "0";
+		return Response.json({
+			status: "idle",
+			lastRun: parseInt(lastRun),
+		});
+	}
+
+	return Response.json({
+		status: "running",
+		host: sessionHostOwner
+	});
+});
+
+app.get("/session/getServer", async (_, env, __, state) => {
+	const user = state.auth[0];
+	const sessionHostOwner = await env.KV.get("sessionHostOwner");
+	if (sessionHostOwner === user) {
+		return new Response((await env.BUCKET.get(state.multipartGlobalKey))?.body);
+	}
+		
+	return new Response(null, { status: 404 });
+});
+
+app.get("/session/start", async (_, env, __, state) => {
+	const sessionHostOwner = await env.KV.get("sessionHostOwner");
+
+	if (sessionHostOwner === null) {
+		await env.KV.put("sessionHostOwner", state.auth[0]);
+
+		return Response.json({
+			status: "started",
+			host: state.auth[0]
+		});
+	}
+
+	return Response.json({
+		status: "running",
+		host: sessionHostOwner,
+	}, { status: 403 });
+});
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const { pathname, searchParams } = new URL(request.url);
-		const auth = request.headers.get("Authorization")?.split(" ");
+		const auth: string[] | undefined = request.headers.get("Authorization")?.split(" ");
 		const multipartGlobalKey = "MCCLServer.tar";
 
-		if (!await checkHosting(auth, env)) {
+		if (!auth || !await checkAuth(auth, env)) {
 			return new Response(null, { status: 401 });
 		}
 
-		const app = new WorkersApp();
+		const state = {
+			auth: auth,
+			multipartGlobalKey: multipartGlobalKey,
+			searchParams: searchParams,
+		};
 
-		app.get("/session/createMultipartUpload", async () => {
-			const user = auth![0];
-			const sessionHostOwner = await env.KV.get("sessionHostOwner");
-
-			if (sessionHostOwner === user) {
-				const multipartUpload = await env.BUCKET.createMultipartUpload(multipartGlobalKey);
-				return Response.json({
-					key: multipartUpload.key,
-					uploadId: multipartUpload.uploadId,
-				});
-			}
-
-			return new Response(null, { status: 404 });
-		});
-
-		app.post("/session/uploadPart", async () => {
-			const user = auth![0];
-			const sessionHostOwner = await env.KV.get("sessionHostOwner");
-			const uploadId = searchParams.get("uploadId"),
-			part = parseInt(searchParams.get("part") ?? "");
-
-			if (sessionHostOwner === user && uploadId && !Number.isNaN(part) && request.body) {
-				const multipartUpload = env.BUCKET.resumeMultipartUpload(multipartGlobalKey, uploadId);
-				const uploadedPart: R2UploadedPart = await multipartUpload.uploadPart(part, request.body);
-
-				return Response.json(uploadedPart);
-			}
-
-			return new Response(null, { status: 404 });
-		}, { startswithCheck: true });
-
-		app.post("/session/uploadComplete", async () => {
-			const user = auth![0];
-			const sessionHostOwner = await env.KV.get("sessionHostOwner");
-			const uploadId = searchParams.get("uploadId");
-
-			if (sessionHostOwner === user && uploadId) {
-				const multipartUpload = env.BUCKET.resumeMultipartUpload(multipartGlobalKey, uploadId);
-
-				await multipartUpload.complete(await request.json());
-
-				return new Response();
-			}
-
-			return new Response(null, { status: 404 });
-		}, { startswithCheck: true });
-
-		app.get("/session/uploadAbort", async () => {
-			const user = auth![0];
-			const sessionHostOwner = await env.KV.get("sessionHostOwner");
-			const uploadId = searchParams.get("uploadId");
-
-			if (sessionHostOwner === user && uploadId) {
-				const multipartUpload = env.BUCKET.resumeMultipartUpload(multipartGlobalKey, uploadId);
-
-				await multipartUpload.abort();
-
-				return new Response();
-			}
-
-			return new Response(null, { status: 404 });
-		}, { startswithCheck: true });
-
-		app.get("/session/stop", async () => {
-			const user = auth![0];
-			const sessionHostOwner = await env.KV.get("sessionHostOwner");
-
-			if (sessionHostOwner === user) {
-				await env.KV.delete("sessionHostOwner");
-	
-				const time = Math.floor(Date.now() / 1000).toString();
-				await env.KV.put("lastRun", time);
-	
-				return Response.json({ time: time });
-			}
-
-			return new Response(null, { status: 404 });
-		});
-
-		app.get("/session/check", async () => {
-			const sessionHostOwner = await env.KV.get("sessionHostOwner");
-			if (sessionHostOwner === null) {
-				const lastRun = await env.KV.get("lastRun") ?? "0";
-				return Response.json({
-					status: "idle",
-					lastRun: parseInt(lastRun),
-				});
-			}
-	
-			return Response.json({
-				status: "running",
-				host: sessionHostOwner
-			});
-		});
-
-		app.get("/session/getServer", async () => {
-			const user = auth![0];
-			const sessionHostOwner = await env.KV.get("sessionHostOwner");
-			if (sessionHostOwner === user) {
-				return new Response((await env.BUCKET.get(multipartGlobalKey))?.body);
-			}
-				
-			return new Response(null, { status: 404 });
-		});
-
-		app.get("/session/start", async () => {
-			const sessionHostOwner = await env.KV.get("sessionHostOwner");
-			if (sessionHostOwner === null) {
-				await env.KV.put("sessionHostOwner", auth![0]);
-	
-				return Response.json({
-					status: "started",
-					host: auth![0]
-				});
-			}
-	
-			return Response.json({
-				status: "running",
-				host: sessionHostOwner,
-			}, { status: 403 });
-		});
-
-		return app.handle(pathname, request.method);
+		return app.handle(request, env, ctx, state);
 	},
 };
